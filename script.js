14579fb907a581a86e321c56d75bdfbae83ab31e
MariusVasie <49002727+MariusVasie@users.noreply.github.com>
2024-10-01 08:00:10 +0300

    Begin setting up the hover action

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-// Sets important constants and variables
+// Constants and variables
 
 const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
-// Creates a default grid sized 16x16
+// Create a default grid sized 16x16
 function defaultGrid() {
     makeRows(16);
     makeColumns(16);
 }
 
-// Takes (rows, columns) input and makes a grid
 function makeRows(rowNum) {
-
-    // Creates rows
     for (r = 0; r < rowNum; r++) {
         let row = document.createElement("div");
         container.appendChild(row).className = "gridRow";
     };
 };
 
-// Creates columns
 function makeColumns(cellNum) {
     for (i = 0; i < rows.length; i++) {
         for (j = 0; j < cellNum; j++) {
@@ -32,3 +28,7 @@ function makeColumns(cellNum) {
 };
 
 defaultGrid();
+
+container.addEventListener("hover", function (e) {
+    cells[1].classList.add("hover");
+  });
